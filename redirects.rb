f700a6d7f04b01598c093d75d4e6c5f8aab12d11
radaeze <33758858+radaeze@users.noreply.github.com>
2018-09-04 17:19:08 -0700

    add version number [#160005448]

--- redirects.rb
+++ redirects.rb
@@ -3,4 +3,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 r301 %r{/telemetry/(?![\d-]+)(.*)}, "/telemetry/$1"
-r301 '/', '/telemetry/index.html'
+r301 '/', '/telemetry/0-5/index.html'
