fb91146ae20a6a23845bc14af282bac95bb47e8a
Landon Jarzynski <ljarzynski@pivotal.io>
2019-04-18 09:55:55 -0700

    adds telemetry 0.7

--- redirects.rb
+++ redirects.rb
@@ -2,5 +2,5 @@
 r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
-r301 %r{/telemetry/(?![\d-]+)(.*)}, "/telemetry/0-6/$1"
-r301 '/', '/telemetry/0-6/index.html'
+r301 %r{/telemetry/(?![\d-]+)(.*)}, "/telemetry/0-7/$1"
+r301 '/', '/telemetry/0-7/index.html'
