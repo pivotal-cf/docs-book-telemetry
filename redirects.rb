01068949dd58a320b249bc92dad23c97e6ed3ed3
Matt Campbell <macampbell@vmware.com>
2021-11-17 11:52:24 -0500

    Update docs book to point to 1.2 instead of 1.1

--- redirects.rb
+++ redirects.rb
@@ -2,5 +2,5 @@
 r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
-r301 %r{/telemetry/(?![\d-]+)(.*)}, "/telemetry/1-1/$1"
-r301 '/', '/telemetry/1-1/index.html'
+r301 %r{/telemetry/(?![\d-]+)(.*)}, "/telemetry/1-2/$1"
+r301 '/', '/telemetry/1-2/index.html'
